bc4a3c72187b80950eb403edb080546ab4f60f85
Miroslav <Miroslav@Bzucko>
2014-11-04 16:04:33 +0000

    This will smash you :)

---
+++ src/Miroslav.java
@@ -0,0 +1,8 @@
+
+public class Miroslav {
+	public static void main(String[] args) {
+      Student fourthStudent = new Student("Miroslav", 27, 'M', "Computer Science");
+		fourthStudent.displayStudentDetails();
+	}
+
+}
